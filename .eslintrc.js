ddd7e6f884001b5aa1d6182cd2d273ff173a8618
uma <sarapan19980221@gmail.com>
2022-12-17 08:45:53 +0800

    feat: template initialize

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['ConditionalExpression'],
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function Router() {
+  return (
+    <div className={styles.container}>
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default Router;
