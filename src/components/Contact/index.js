35d392bbee8d93bbfca8c6d2e19c0f1122a4e37d
uma <sarapan19980221@gmail.com>
2022-12-20 16:29:13 +0800

    feat: contact link & css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import titleSkillsPNG from '../../asset/imgs/BTitle_SKILLS.png';
+import titleContactPNG from '../../asset/imgs/CTitle_CONTACT.png';
+import iconEmailPNG from '../../asset/imgs/Icon_Email.png';
+import iconInstagramPNG from '../../asset/imgs/Icon_Instagram.png';
+import iconTelegramPNG from '../../asset/imgs/Icon_Telegram.png';
 
 function Contact() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <img className={styles.contact_title} src={titleSkillsPNG}></img>
+        <img className={styles.contact_title} src={titleContactPNG}></img>
         <div className={styles.contact_text}>
           想進一步了解 / 合作嗎？歡迎以下方聯絡方式
         </div>
         <div className={styles.contact_nav}>
-          <div className={styles.contact_circle}></div>
-          <div className={styles.contact_circle}></div>
-          <div className={styles.contact_circle}></div>
-          <div className={styles.contact_circle}></div>
+          <div
+            className={`${styles.contact_circle} ${styles.contact_personalPhoto}`}
+          ></div>
+          <a
+            href="mailto:eoe172@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.contact_circle} ${styles.contact_link}`}
+          >
+            <div></div>
+            <img src={iconEmailPNG}></img>
+          </a>
+          <a
+            href="https://instagram.com/motiondesigner_edwin?igshid=YmMyMTA2M2Y="
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.contact_circle} ${styles.contact_link}`}
+          >
+            <div></div>
+            <img src={iconInstagramPNG}></img>
+          </a>
+          <a
+            href="https://t.me/EdwinHuang_0213"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.contact_circle} ${styles.contact_link}`}
+          >
+            <div></div>
+            <img src={iconTelegramPNG}></img>
+          </a>
         </div>
       </div>
     </div>
