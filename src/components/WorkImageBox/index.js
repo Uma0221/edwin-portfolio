f9f4c2f4da38ac9a99e665165bdee57b0cb1b00b
uma <sarapan19980221@gmail.com>
2023-01-14 04:32:06 +0800

    feat: workPage carouselBox

--- src/components/WorkImageBox/index.js
+++ src/components/WorkImageBox/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 
 import { StoreContext } from '../../store/reducer';
@@ -10,12 +10,124 @@ function WorkInfoBox() {
     state: { portfolioNavState, workState },
   } = useContext(StoreContext);
 
+  const [carouselOldIndex, setcarouselOldIndex] = useState(0);
+  const [carouselClickIndex, setcarouselClickIndex] = useState(0);
+
   useEffect(() => {
     window.scrollTo(top);
-  }, []);
+    setcarouselOldIndex(0);
+    setcarouselClickIndex(0);
+  }, [workState]);
+
+  useEffect(() => {
+    if (worksImageJson[portfolioNavState]?.works[workState]?.pictures) {
+      const count = setInterval(() => {
+        setcarouselOldIndex(carouselClickIndex);
+        if (
+          carouselClickIndex ==
+          worksImageJson[portfolioNavState]?.works[workState]?.pictures.length -
+            1
+        ) {
+          setcarouselClickIndex(0);
+        } else {
+          setcarouselClickIndex(carouselClickIndex + 1);
+        }
+      }, 5000);
+      return () => clearInterval(count);
+    }
+  }, [carouselClickIndex]);
 
   return (
     <div className={styles.container}>
+      {worksImageJson[portfolioNavState].works[workState].pictures ? (
+        <>
+          <div className={`${styles.pictureBox} ${styles.carouselBox}`}>
+            {worksImageJson[portfolioNavState].works[workState].pictures.map(
+              (item, index) => (
+                <img
+                  key={`carousel_img${index}`}
+                  className={
+                    carouselClickIndex == index &&
+                    carouselOldIndex == carouselClickIndex
+                      ? `${styles.pictureContent} ${styles.carouselItem} ${styles.noMove}`
+                      : carouselClickIndex == index &&
+                        carouselOldIndex < carouselClickIndex
+                      ? `${styles.pictureContent} ${styles.carouselItem} ${styles.RtoM}`
+                      : carouselClickIndex == index &&
+                        carouselOldIndex > carouselClickIndex
+                      ? `${styles.pictureContent} ${styles.carouselItem} ${styles.LtoM}`
+                      : carouselOldIndex == index &&
+                        carouselOldIndex < carouselClickIndex
+                      ? `${styles.pictureContent} ${styles.carouselItem} ${styles.MtoL}`
+                      : carouselOldIndex == index &&
+                        carouselOldIndex > carouselClickIndex
+                      ? `${styles.pictureContent} ${styles.carouselItem} ${styles.MtoR}`
+                      : `${styles.pictureContent} ${styles.carouselItem}`
+                  }
+                  src={item.url}
+                />
+              ),
+            )}
+          </div>
+
+          <div className={styles.carouselIndicators}>
+            {worksImageJson[portfolioNavState].works[workState].pictures.map(
+              (item, index) => (
+                <button
+                  key={`carousel_btn${index}`}
+                  className={
+                    carouselClickIndex == index
+                      ? `${styles.circle} ${styles.select}`
+                      : styles.circle
+                  }
+                  onClick={() => {
+                    setcarouselOldIndex(carouselClickIndex);
+                    setcarouselClickIndex(index);
+                  }}
+                ></button>
+              ),
+            )}
+          </div>
+
+          <div className={`${styles.captionBox} ${styles.carouselBox}`}>
+            {worksImageJson[portfolioNavState].works[workState].pictures.map(
+              (item, index) => (
+                <div
+                  key={`carousel_caption${index}`}
+                  className={
+                    carouselClickIndex == index &&
+                    carouselOldIndex == carouselClickIndex
+                      ? `${styles.captionContent} ${styles.carouselItem} ${styles.noMove}`
+                      : carouselClickIndex == index &&
+                        carouselOldIndex < carouselClickIndex
+                      ? `${styles.captionContent} ${styles.carouselItem} ${styles.RtoM}`
+                      : carouselClickIndex == index &&
+                        carouselOldIndex > carouselClickIndex
+                      ? `${styles.captionContent} ${styles.carouselItem} ${styles.LtoM}`
+                      : carouselOldIndex == index &&
+                        carouselOldIndex < carouselClickIndex
+                      ? `${styles.captionContent} ${styles.carouselItem} ${styles.MtoL}`
+                      : carouselOldIndex == index &&
+                        carouselOldIndex > carouselClickIndex
+                      ? `${styles.captionContent} ${styles.carouselItem} ${styles.MtoR}`
+                      : `${styles.captionContent} ${styles.carouselItem}`
+                  }
+                >
+                  <div className={styles.captionTitle}>{item.title}</div>
+                  <button className={styles.captionbtn}>查看更多 V </button>
+                  {/* <div className={styles.captionInfo}>
+                BlueRemedy
+                海報以夏天氛圍為設計主色，對應遊戲場景中春天、秋天、冬天三個不同的季節組成四季
+              </div> */}
+                </div>
+              ),
+            )}
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
       {worksImageJson[portfolioNavState].works[workState].videos ? (
         <>
           {worksImageJson[portfolioNavState].works[workState].videos.map(
