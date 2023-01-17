654a0dd99e76c0d54ae8d753d0d08efed32cdac3
uma <sarapan19980221@gmail.com>
2023-01-17 22:40:18 +0800

    feat: banner exp content

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -7,20 +7,65 @@ import { setExpState } from '../../store/actions';
 import expJson from '../../asset/json/exp.json';
 
 function Experience() {
+  var expItemArr = Array(expJson.length).fill(0);
+
   var maxPage = 0;
+  var pageArr = [];
+
+  var pageExpArr = [];
+  var pageYearArr = [];
+
+  for (i = 0; i < expJson.length; i++) {
+    var count2 = 0;
+    for (j = i; j >= 0; j--) {
+      count2 += expJson[j].exp.length;
+      if (j == 0) {
+        expItemArr[i] = count2;
+      }
+    }
+  }
+
+  function pageItem(item) {
+    if (item <= 3) {
+      pageArr.push({ yearFlag: false, item: item });
+    } else {
+      pageArr.push({ yearFlag: true, item: 3 });
+      pageItem(item - 3);
+    }
+  }
 
   expJson.map((year_exp) => {
-    maxPage = maxPage + parseInt((year_exp.exp.length - 1) / 3) + 1;
+    var expLength = parseInt((year_exp.exp.length - 1) / 3) + 1;
+    maxPage += expLength;
+    pageItem(year_exp.exp.length);
   });
+
+  pageExpArr = Array(pageArr.length).fill(0);
+  for (var i = 0; i < pageArr.length; i++) {
+    var count1 = 0;
+    for (var j = i; j >= 0; j--) {
+      count1 += pageArr[j].item;
+      if (j == 0) {
+        pageExpArr[i] = count1;
+      }
+    }
+  }
+
+  pageYearArr = Array(pageExpArr.length).fill(0);
+  for (i = 0; i < pageExpArr.length; i++) {
+    if (pageArr[i].yearFlag) {
+      pageYearArr[i] = pageExpArr[i - 1];
+    } else {
+      pageYearArr[i] = pageExpArr[i];
+    }
+  }
 
   const {
     state: { expState },
     dispatch,
   } = useContext(StoreContext);
 
-  useEffect(() => {
-    console.log(expState);
-  }, [expState]);
+  useEffect(() => {}, [expState]);
 
   return (
     <div className={styles.container}>
@@ -31,30 +76,80 @@ function Experience() {
             <div className={styles.circle}></div>
             <div className={styles.circle}></div>
           </div>
+
           <div className={styles.content}>
             <div className={styles.page}>
-              <div className={styles.yearExp_block}>
-                <div>
-                  {expJson.map((year_exp, index) => (
-                    <div key={`year_exp${index}`} className={styles.year_exp}>
-                      {year_exp.year}
-                    </div>
-                  ))}
-                </div>
-              </div>
+              {expJson.map((year_exp, year_index) => (
+                <div key={`all_exp${year_index}`}>
+                  {year_exp.exp.map((exp, exp_index) => (
+                    <div key={`exp${exp_index}`}>
+                      {exp_index == 0 ? (
+                        <div
+                          className={styles.year_exp}
+                          style={{
+                            top:
+                              year_index > 0
+                                ? expState > 0
+                                  ? `${
+                                      (expItemArr[year_index - 1] +
+                                        exp_index -
+                                        pageYearArr[expState - 1]) *
+                                        76 +
+                                      36
+                                    }px`
+                                  : `${
+                                      (expItemArr[year_index - 1] + exp_index) *
+                                        76 +
+                                      36
+                                    }px`
+                                : expState > 0
+                                ? `${
+                                    (exp_index - pageYearArr[expState - 1]) *
+                                      76 +
+                                    36
+                                  }px`
+                                : `${exp_index * 76 + 36}px`,
+                          }}
+                        >
+                          {year_exp.year}
+                        </div>
+                      ) : (
+                        <></>
+                      )}
 
-              <div className={styles.all_exp}>
-                {expJson.map((year_exp, index) => (
-                  <div key={`all_exp${index}`}>
-                    {year_exp.exp.map((exp, index) => (
-                      <div key={`exp${index}`} className={styles.exp}>
+                      <div
+                        className={styles.exp}
+                        style={{
+                          top:
+                            year_index > 0
+                              ? expState > 0
+                                ? `${
+                                    (expItemArr[year_index - 1] +
+                                      exp_index -
+                                      pageExpArr[expState - 1]) *
+                                      76 +
+                                    36
+                                  }px`
+                                : `${
+                                    (expItemArr[year_index - 1] + exp_index) *
+                                      76 +
+                                    36
+                                  }px`
+                              : expState > 0
+                              ? `${
+                                  (exp_index - pageExpArr[expState - 1]) * 76 +
+                                  36
+                                }px`
+                              : `${exp_index * 76 + 36}px`,
+                        }}
+                      >
                         <div className={styles.exp_line}>{exp.line1}</div>
                         <div className={styles.exp_line}>{exp.line2}</div>
                       </div>
-                    ))}
-                  </div>
-                ))}
-              </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
             </div>
 
             <div className={styles.controler}>
