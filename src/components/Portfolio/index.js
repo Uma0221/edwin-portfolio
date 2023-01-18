844151b5bf8ad343a9ea591f29537e44a9229136
uma <sarapan19980221@gmail.com>
2023-01-18 19:23:55 +0800

    feat: portfolio works page change

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -12,6 +12,7 @@ import {
 } from '../../store/actions';
 
 import worksJson from '../../asset/json/works.json';
+import triangleImg from '../../asset/imgs/banner/triangle1.png';
 
 function Portfolio() {
   const {
@@ -19,8 +20,18 @@ function Portfolio() {
     dispatch,
   } = useContext(StoreContext);
 
+  const [visibaleWidth, setVisibaleWidth] = useState(0);
+  const [maxWorks, setMaxWorks] = useState(0);
+  const [pagesCount, setpagesCount] = useState(0);
+  const [pageIndex, setPageIndex] = useState(0);
+  const [noAnimation, setNoAnimation] = useState(false);
+
   const [imgURLArr, setImgURLArr] = useState(null);
   const [imgLoadState, setImgLoadState] = useState(false);
+
+  let resizeWindow = () => {
+    setVisibaleWidth(window.innerWidth - 96);
+  };
 
   function preloadImage(url) {
     return new Promise((resolve, reject) => {
@@ -39,6 +50,18 @@ function Portfolio() {
     const imagePromiseArr = arr.map(preloadImage);
     return Promise.all(imagePromiseArr);
   }
+
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener('resize', resizeWindow);
+    return () => {
+      window.removeEventListener('resize', resizeWindow);
+    };
+  }, []);
+
+  useEffect(() => {
+    setMaxWorks(parseInt(visibaleWidth / 340));
+  }, [visibaleWidth]);
 
   useEffect(() => {
     setImgLoadState(true);
@@ -64,6 +87,20 @@ function Portfolio() {
     }
   }, [imgURLArr && imgURLArr[0]]);
 
+  useEffect(() => {
+    setNoAnimation(true);
+    setPageIndex(0);
+
+    if (maxWorks) {
+      setpagesCount(
+        parseInt((worksJson[portfolioNavState].works.length - 1) / maxWorks) +
+          1,
+      );
+    }
+  }, [portfolioNavState, maxWorks]);
+
+  useEffect(() => {}, [pagesCount, pageIndex]);
+
   return (
     <div className={styles.container}>
       <div className={styles.nav}>
@@ -88,29 +125,94 @@ function Portfolio() {
       </div>
 
       <div className={styles.works}>
+        <div className={styles.controller}>
+          <button
+            className={
+              pageIndex == 0
+                ? `${styles.triangle} ${styles.triangle_rotate} ${styles.notshow}`
+                : `${styles.triangle} ${styles.left}`
+            }
+            onClick={() => {
+              setNoAnimation(false);
+              setPageIndex(pageIndex - 1);
+            }}
+          >
+            <img src={triangleImg}></img>
+          </button>
+
+          <div
+            className={
+              pageIndex == pagesCount - 1
+                ? `${styles.trans_cover} ${styles.right} ${styles.notshow}`
+                : `${styles.trans_cover} ${styles.right}`
+            }
+          ></div>
+
+          <button
+            className={
+              pageIndex == pagesCount - 1
+                ? `${styles.triangle} ${styles.right} ${styles.notshow}`
+                : `${styles.triangle} ${styles.right}`
+            }
+            onClick={() => {
+              setNoAnimation(false);
+              setPageIndex(pageIndex + 1);
+            }}
+          >
+            <img src={triangleImg}></img>
+          </button>
+        </div>
+
         {worksJson[portfolioNavState].works.map((work, index) => (
-          <Link
-            to={`/Portfolio/${worksJson[portfolioNavState].collection}?index=${index}`}
+          <div
             key={`work_${index}`}
             className={styles.work}
+            style={{
+              transform: `translateX(${-pageIndex * maxWorks * 340}px)`,
+              transition: noAnimation ? 'none' : '1s ease-in-out',
+            }}
           >
-            <div className={styles.work_bg}>
-              {imgLoadState ? (
-                ''
-              ) : (
-                <img className={styles.work_img} src={work.imgURL}></img>
-              )}
+            <Link
+              to={`/Portfolio/${worksJson[portfolioNavState].collection}?index=${index}`}
+            >
+              <div className={styles.work_bg}>
+                {imgLoadState ? (
+                  ''
+                ) : (
+                  <img className={styles.work_img} src={work.imgURL}></img>
+                )}
 
-              <div className={styles.work_name}>{work.name}</div>
-              <div className={styles.work_cover}>
-                <div className={styles.work_intro}>{work.intro}</div>
-                <div className={styles.work_more}>MORE</div>
+                <div className={styles.work_name}>{work.name}</div>
+                <div className={styles.work_cover}>
+                  <div className={styles.work_intro}>{work.intro}</div>
+                  <div className={styles.work_more}>MORE</div>
+                </div>
               </div>
-            </div>
-
+            </Link>
             <div className={styles.work_keywords}>{work.keywords}</div>
-          </Link>
+          </div>
         ))}
+      </div>
+
+      <div className={styles.pages}>
+        {pagesCount > 1 ? (
+          [...Array(pagesCount)].map((page, index) => (
+            <button
+              key={`page${index}`}
+              className={
+                pageIndex == index
+                  ? `${styles.circle} ${styles.select}`
+                  : styles.circle
+              }
+              onClick={() => {
+                setNoAnimation(false);
+                setPageIndex(index);
+              }}
+            ></button>
+          ))
+        ) : (
+          <div style={{ height: '16px' }}></div>
+        )}
       </div>
     </div>
   );
