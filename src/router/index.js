e81a63f6fd4e5af1f811b7159f1175c868a746d0
uma <sarapan19980221@gmail.com>
2023-01-18 19:01:54 +0800

    fix: load page when browser back button click

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StoreProvider } from '../store/reducer';
 
@@ -8,6 +8,17 @@ import HomePage from '../layouts/HomePage';
 import WorkPage from '../layouts/WorkPage';
 
 function Router() {
+  let lastPage = () => {
+    window.location.replace(location.href);
+  };
+
+  useEffect(() => {
+    window.addEventListener('popstate', lastPage);
+    return () => {
+      window.removeEventListener('popstate', lastPage);
+    };
+  }, []);
+
   return (
     <StoreProvider>
       <BrowserRouter>
